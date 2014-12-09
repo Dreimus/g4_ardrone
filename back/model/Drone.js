820f45d045ab32cefcd9c4429b439c7f7efd9818
Firnen <victor.ackermann.30@gmail.com>
2014-12-09 09:20:44 +0100

    Drone
    fonctionInit

--- back/model/Drone.js
+++ back/model/Drone.js
@@ -6,9 +6,19 @@ drone.takeoff();
 drone
 	.after(4000, function()	{
 		this.clockwise(0.2); //tourne sur lui même
-		this.left(0.01); // ..(speed)
+		this.left(0.01); // ..(speed) 0.01=1m en distance.
 	})
 	.after(2000, function() {
 		this.stop();
 		this.land();
 	});
+	
+//initialisation du drone
+functionInit(){
+
+drone
+	.after(1000, function(){
+		this.calibrate(0); // fait une calibration au decollage, provoque un 360 brusque.
+		this.animateLeds('blinkGreenOrangeRed',5,5)// anime les leds a 5Hz pendant 5s.
+	});
+}
