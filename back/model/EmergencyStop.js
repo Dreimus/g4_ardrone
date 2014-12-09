57ed6edc17c2efd8da34ab360c6df9cf260d7ee2
Xirtam38 <guillaume.regnier@calyce.fr>
2014-12-09 16:11:18 +0100

    Emergency stop

---
+++ back/model/EmergencyStop.js
@@ -0,0 +1,9 @@
+var arDrone = require('ar-drone');
+var drone = arDrone.createClient();
+
+/* --- Fonction d'arrêt d'urgence --- */
+function emergency_stop() {
+	console.log('arrêt d\'urgence !');
+	.drone.stop();
+	.drone.land();
+}
