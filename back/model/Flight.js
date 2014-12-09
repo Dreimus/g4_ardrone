81023e3989cc7fc77aedc1957756791d32a51094
Firnen <victor.ackermann.30@gmail.com>
2014-12-09 14:05:32 +0100

    pour les test

--- back/model/Flight.js
+++ back/model/Flight.js
@@ -14,11 +14,8 @@ drone
 	.after(1000, function(){
 		console.log('Initialisation');
 		this.calibrate(0); // calibre le drone au decolage, provoque un 360.
-		this.animateLeds('blinkGreenOrangeRed',5,5); // fait clignoter les leds pour verif fonctionement.
-		videostream = this.getVideoStream(); // recupere la video.
-		controller.sendStream(videostream);
-	});
-	
+		this.animateLeds('blinkGreenOrangeRed',5,5); // fait clignoter les leds pour verif fonctionement.		
+	});	
 }
 
 function getHightMax(){
