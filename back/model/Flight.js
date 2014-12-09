3408971da8990d94ba32ec8d145d79da97132fc6
Firnen <victor.ackermann.30@gmail.com>
2014-12-09 15:18:49 +0100

    start&stop
    ajoue de fonction

--- back/model/Flight.js
+++ back/model/Flight.js
@@ -1,33 +1,59 @@
+
 var arDrone = require('ar-drone');
 var drone = arDrone.createClient();
-var hightmax;
+var eventEmitter = require('events').EventEmitter
+var hightMax;
 
-drone.takeoff();
 
 
+//=======================================================================================
 // fonction d'initialisation au demarage.	
-function init(){
-var videostream;
-higtmax=180;
+function init(hightMax:180){
 
 drone
-	.after(1000, function(){
+	.after(2000, function(){
 		console.log('Initialisation');
-		this.calibrate(0); // calibre le drone au decolage, provoque un 360.
-		this.animateLeds('blinkGreenOrangeRed',5,5); // fait clignoter les leds pour verif fonctionement.		
-	});	
+		this.animateLeds('blinkRedOrangeGreen',5,5); // fait clignoter les leds pour verif fonctionement.		
+	});
+}.eventEmitter.emit("drone_init");
+
+//======================================================================================
+
+function execute(){
+
+
 }
+
+//=======================================================================================
+// décollage!
+function startFlight(){
+drone.takeoff();
+
+drone
+	.after(2000, function(){
+		this.calibrate(0); // calibre le drone au decolage, provoque un 360.
+		this.animateLeds('green',5,5);		
+	};
+}.eventEmitter.emit("drone_start");
+
+//=======================================================================================
+// Atterissage!
+function stopFlight(){
+drone
+	.after(1000, function() {
+		this.stop();
+		this.land();
+	};
+}.eventEmitter.emit("stop_start");
+
+//=======================================================================================
 
 function getHightMax(){
-
 return hightmax;
 }
-
 function setHightMax(hight){
 hightmax=hight;
-
 }
-
 export.getHightMax=hightmax;
 
-
+//=======================================================================================
