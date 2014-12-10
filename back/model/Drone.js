1d0197d7654bae8c74ceeb725c75197c77fa4bf5
Dreimus <dreimus.contact@gmail.com>
2014-12-10 11:15:18 +0100

    Resovle conflict

--- back/model/Drone.js
+++ back/model/Drone.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-var readable = getReadableStreamSomehow();
-=======
 /*-------------------------------------------------*\
 				DRONE.JS
 
@@ -12,72 +9,95 @@ var readable = getReadableStreamSomehow();
 \*-------------------------------------------------*/
 
 
-/* var arDrone = require('ar-drone');
-var dronev1 = arDrone.createClient(); */
+var arDrone = require('ar-drone');
+var drone = arDrone.createClient(); /*
 var autonomy = require('ardrone-autonomy');
-var drone = autonomy.createMission();
+var drone = autonomy.createMission(); */
 
 
 function Drone(heightMax) {
 	var heightMax, currentHeight;
 	var self = this;
-	var plan = null;
+	var plan = undefined;
+	console.log(' DEBUG - Drone_const');
 	if(heightMax === undefined){
 		heightMax = 180;
 	}
-	setHeightMax = heightMax;
-	/* dronev1.after(2000, function(){
+	setHeightMax = heightMax; /*
+	drone.after(2000, function(){
 			console.log('Initialisation');
-			this.animateLeds('blinkGreenRed',5,5); // Leds blinking		
-		});
-*/
+			this.animateLeds('blinkGreenRed',5,5); // Leds blinking
+		});*/
 	eventEmitter.emit('drone_init');
 }
 
 Drone.prototype.setPlan = function (plan) {
+
 	if( plan === undefined ){
 		console.log('Plan cannot be found.')
 	}else {
 		this.plan = plan;
 	}
+
+	
+	console.log(this.plan);
 }
 
 // - Action Array Analysis and post-action treatment
 
 Drone.prototype.execute = function (plan){
+
 	if( plan !== undefined) {
 		this.plan = plan;
-}	
+	}	
+	
 	var actionList=this.plan.getFlyActionList();
-
-	for (var index = 0; index < actionList.lenght; index++) {
+	console.log(this.plan);
+	
+	for (var index = 0; index < actionList.length; index++) {
+		
 		switch(actionList[index].type){
 			case ETypeAction_file.ETypeAction.Move : 				
 				switch(actionList[index].direction){
 					case EDirection_file.EDirection.Forward :
-					drone.forward( actionList[index].value );
+					drone.after(2000, function(){
+						console.log(actionList[index].value);
+						this.front(  (actionList[index].value / 100 ) );
+					});
 					break;
 
 				case EDirection_file.EDirection.Backward :
-					drone.backward( actionList[index].value );
+					drone.after(2000, function (){
+						this.front(  (actionList[index].value / 100 ) );
+					});
 					break;
 
 				case EDirection_file.EDirection.Left :
-					drone.left( actionList[index].value );
+					drone.after(6000, function (){
+						this.counterClockwise(0.2);
+						this.front(  (actionList[index].value / 100 ) );
+					});
 					break;
 
 				case EDirection_file.EDirection.Right :
-					drone.right( actionList[index].value );
+					drone.after(6000, function (){
+						this.clockwise(0.2);
+						this.front(  (actionList[index].value / 100 ) );
+					});
 					break;
 
 				case EDirection_file.EDirection.Up :
-					if( ( currentHeight + actionList[index].value ) < getHeightMax){
-					drone.up( actionList[index].value );
+					if( ( this.currentHeight + actionList[index].value ) < this.getMaxHeight){
+						drone.after(2000, function() {
+							this.up(  (actionList[index].value / 100 ) );
+						});
 					}
 					break;
 
 				case EDirection_file.EDirection.Down :
-					drone.down( actionList[index].value );
+					drone.after(2000, function() {
+						this.down(  (actionList[index].value / 100 ) );
+					});
 					break;
 
 				default: 
@@ -89,11 +109,15 @@ Drone.prototype.execute = function (plan){
 				switch(actionList[index].direction){
 
 					case EDirection_file.EDirection.Left :
-						drone.cw(actionList[index].value);
+						drone.after(2000, function() {
+							this.clockwise( (actionList[index].value / 100 ));
+						});
 						break;
 
 					case EDirection_file.EDirection.Right :
-						drone.ccw(actionList[index].value);
+						drone.after(2000, function() {
+							this.counterClockwise( (actionList[index].value / 100 ));
+						});
 						break;
 						
 					default: 
@@ -103,8 +127,8 @@ Drone.prototype.execute = function (plan){
 
 		default: 
 			console.log("Erreur de traitement de l'action"+index);
-	}
-	eventEmitter.emit('drone.action');
+		}
+	eventEmitter.emit('drone_action');
 	}
 }
 
@@ -124,50 +148,19 @@ Drone.prototype.startFlight = function(){
 
 // - Land order
 Drone.prototype.stopFlight = function (){
-	drone.land();		
+	drone.stop();
+	drone.land();	
 	eventEmitter.emit('drone_stop');
 }
 
 // - Height getter
-Drone.prototype.getHeightMax = function(){
+Drone.prototype.getMaxHeight = function(){
 	return heightMax;
 }
 
 // - Height setter
-Drone.prototype.setHeightMax = function(height){
+Drone.prototype.setMaxHeight = function(height){
 	heightMax=height;
 }
 
 module.exports = Drone;
-
-//=======================================================================================
-/* 
-
-		DEPRECACED SCRIPT
-
-var arDrone = require('ar-drone');
-var drone = arDrone.createClient();
->>>>>>> 82e235383f24f6e444503d60997778783317217a
-
-function getStream() {
-
-}
-
-<<<<<<< HEAD
-function stream() {
-
-}
-
-=======
-*/
-
-/* --- fonction d'arrêt d'urgence --- */
-
-// function emergency_stop() {
-// 	console.log('arrêt d\'urgence !!');
-// 	.drone.stop();
-// 	.drone.land();
-// }
-
-
->>>>>>> 82e235383f24f6e444503d60997778783317217a
